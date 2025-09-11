1e770a00c86720556bf4a42e873e6e3ce2554348
issathanh <94776785+issathanh@users.noreply.github.com>
2025-09-10 22:20:44 -0500

    adding css

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,26 +1,36 @@
-import { Text, View,Image } from "react-native";
+import { Text, View,Image,StyleSheet } from "react-native";
 import { Link } from "expo-router";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
       <Text>Edit app/index.tsx to edirt this screen.</Text>
       <Link href="./about">Go to About</Link>
 
       <Image 
       source= {{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}}
-      style={{width:200,height:200}}
+      style={styles.image}
       />  
 
       <Image 
       source = {require('@/assets/images/icon.png')}
-      style={{width:200,height:200}}
+      style={styles.image}
       />
     </View>
   );
 }
+//view == div
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "purple"
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+})
